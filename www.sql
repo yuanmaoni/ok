d5b79448acf7ceb1715dc779a2b06eaeece0ac9c
RYAN REN <iamryanren@gmail.com>
2022-06-13 11:17:27 +0900

    ok

---
+++ www.sql
@@ -0,0 +1,14 @@
+create table student01
+(sid char(2),sname varchar(255),sage date,ssex varchar(255));
+insert into student01 values('01','趙雷','1990-01-01','男');
+insert into student01 values('02','??','1990-12-21','男');
+insert into student01 values('03','??','1990-12-20','男');
+insert into student01 values('04','李云','1990-12-06','男');
+insert into student01 values('05','周梅','1991-12-01','女');
+insert into student01 values('06','??','1992-01-01','女');
+insert into student01 values('07','?竹','1989-01-01','女');
+insert into student01 values('09','?三','2017-12-20','女');
+insert into student01 values('10','李四','2017-12-25','女');
+insert into student01 values('11','李四','2012-06-06','女');
+insert into student01 values('12','?六','2013-06-13','女');
+insert into student01 values('13','?七','1989-01-01','女');
